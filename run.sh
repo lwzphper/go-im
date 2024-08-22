2dea727106ab4da2c08b6e4389a6c7cf32365ee3
劳伟臻 <1149243551@qq.com>
2024-08-22 16:21:45 +0800

    连接层与逻辑层代码分离

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ go build -o ./dist/service ./cmd/service/main.go
 echo "打包 server 成功"
 pkill service
 echo "停止 server 服务"
-nohup ./dist/service -a :8080 & 2>/dev/null
+nohup ./dist/service -a :8080 -f ./etc/config.yaml & 2>/dev/null
 echo "启动 server 8080 服务"
-nohup ./dist/service -a :8081 & 2>/dev/null
+nohup ./dist/service -a :8081 -f ./etc/config.yaml & 2>/dev/null
 echo "启动 server 8081 服务"
 
 rm -f dist/gateway
@@ -23,5 +23,5 @@ go build -o ./dist/gateway ./cmd/gateway/main.go
 echo "打包 gateway 成功"
 pkill gateway
 echo "停止 gateway 服务"
-nohup ./dist/gateway & 2>/dev/null
+nohup ./dist/gateway -f ./etc/config.yaml & 2>/dev/null
 echo "启动 gateway 服务"
